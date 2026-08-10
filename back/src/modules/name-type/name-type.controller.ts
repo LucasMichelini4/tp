6c7db5854fb47d55d725bde64ceaab54955a44f8
LucasMichelini4 <lucasmichelini1846@gmail.com>
2026-08-10 09:16:09 -0300

    Name Type terminado

--- back/src/modules/name-type/name-type.controller.ts
+++ back/src/modules/name-type/name-type.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { NameTypeService } from './name-type.service.js';
+import { NameTypeDto } from './dto/name-type-dto.js';
 
 @Controller('api/v1/name-type')
-export class NameTypeController { }
+@ApiTags('NameType')
+export class NameTypeController {
+  constructor(private nameTypeService: NameTypeService) { }
+
+  @Post()
+  createClient(@Body() nameType: NameTypeDto) {
+    return this.nameTypeService.createNameType(nameType)
+  }
+
+  @Get(':id')
+  getNameTypeById(@Param('id') id: number) {
+    return this.nameTypeService.findNameType(id);
+  }
+
+  @Get()
+  getNameTypes() {
+    return this.nameTypeService.findAll();
+  }
+
+  @Get('filter/deleted')
+  getNameTypeDeleted() {
+    return this.nameTypeService.findAllDeleted();
+  }
+
+  @Put()
+  updateNameType(@Body() nameType: NameTypeDto) {
+    return this.nameTypeService.updateNameType(nameType);
+  }
+
+  @Delete('/:id')
+  deleteNameType(@Param('id') id: number) {
+    return this.nameTypeService.deleteNameType(id);
+  }
+
+  @Patch('/restore/:id')
+  restoreNameType(@Param('id') id: number) {
+    return this.nameTypeService.restoreNameType(id);
+  }
+}
--- back/src/modules/name-type/name-type.service.ts
+++ back/src/modules/name-type/name-type.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { NameType } from './entity/name-type.entity.js';
-import { Repository } from 'typeorm';
+import { Repository, UpdateResult } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import { NameTypeDto } from './dto/name-type-dto.js';
 
 @Injectable()
 export class NameTypeService {
@@ -9,4 +10,52 @@ export class NameTypeService {
     @InjectRepository(NameType) private nameTypeRepository: Repository<NameType>
   ) { }
 
+  async createNameType(nameType: NameTypeDto) {
+    const nameTypeExists = await this.findNameType(nameType.id);
+    if (nameTypeExists) {
+      throw new ConflictException('El nameType con id: ' + nameType.id + ' existe')
+    } else {
+      return await this.nameTypeRepository.save(nameType)
+    }
+  }
+
+  async findNameType(id: number) {
+    return await this.nameTypeRepository.findOne({ where: { id } });
+  }
+
+  async findAll() {
+    return await this.nameTypeRepository.find({ where: { deleted: false } });
+  }
+
+  async findAllDeleted() {
+    return await this.nameTypeRepository.find({ where: { deleted: true } });
+  }
+
+  async updateNameType(nameType: NameTypeDto) {
+    return await this.nameTypeRepository.save(nameType);
+  }
+
+  async deleteNameType(id: number) {
+    const nameTypeExists = await this.findNameType(id);
+    if (!nameTypeExists) {
+      throw new ConflictException('El nameType con id: ' + id + ' no existe')
+    }
+    if (nameTypeExists.deleted) {
+      throw new ConflictException('El nameType ya esta borrado')
+    }
+    const rows: UpdateResult = await this.nameTypeRepository.update({ id }, { deleted: true });
+    return rows.affected == 1;
+  }
+
+  async restoreNameType(id: number) {
+    const nameTypeExists = await this.findNameType(id);
+    if (!nameTypeExists) {
+      throw new ConflictException('El nameType con id: ' + id + ' no existe')
+    }
+    if (!nameTypeExists.deleted) {
+      throw new ConflictException('El nameType no esta borrado')
+    }
+    const rows: UpdateResult = await this.nameTypeRepository.update({ id }, { deleted: false });
+    return rows.affected == 1;
+  }
 }
